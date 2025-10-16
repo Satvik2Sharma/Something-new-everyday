1704e91df4dcc3910538c8f1c4a084b606213b5d
Satvik2Sharma <satviksharma1706@gmail.com>
2025-10-16 21:55:00 +0530

    Create remove_vowels.py
    S = input("Enter a string: ")
    
    vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
    
    # Using For loop , lower() method to determine the vowels.
    
    result = S # Initialize result with the original string.
    
    for i in S: # Iterate through the original string to preserve order.
    
     if i in vowels:
    
    # Using replace method to remove vowels.
    
      result = result.replace(i, "")
    
    print("After removing vowels", result)

---
+++ remove_vowels.py
@@ -0,0 +1,17 @@
+S = input("Enter a string: ")
+
+vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+
+# Using For loop , lower() method to determine the vowels.
+
+result = S # Initialize result with the original string.
+
+for i in S: # Iterate through the original string to preserve order.
+
+ if i in vowels:
+
+# Using replace method to remove vowels.
+
+  result = result.replace(i, "")
+
+print("After removing vowels", result)
