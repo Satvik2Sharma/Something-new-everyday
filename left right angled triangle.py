73c5a2052765231e44305f22464e18fca6a8cdc5
Satvik2Sharma <satviksharma1706@gmail.com>
2025-10-18 09:04:47 +0530

    Fix comment casing in triangle printing script

--- left right angled triangle.py
+++ left right angled triangle.py
@@ -1,4 +1,4 @@
-# Using for loop to print a right angled triangle using asterisk(*) of any height.
+# using for loop to print a right angled triangle using asterisk(*) of any height.
 
 n = int(input("Enter height: "))
 for i in range(1, n+1):
