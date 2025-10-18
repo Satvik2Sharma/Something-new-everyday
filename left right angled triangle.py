143b0b8239c020f81132061f8f3fbc955b6a6746
Satvik2Sharma <satviksharma1706@gmail.com>
2025-10-18 09:07:22 +0530

    left sided right angled asterisk triangle.py
    Using for loop to print a right angled triangle using asterisk(*) of any height.

--- left right angled triangle.py
+++ left right angled triangle.py
@@ -1,4 +1,4 @@
-# using for loop to print a right angled triangle using asterisk(*) of any height.
+# Using for loop to print a right angled triangle using asterisk(*) of any height.
 
 n = int(input("Enter height: "))
 for i in range(1, n+1):
