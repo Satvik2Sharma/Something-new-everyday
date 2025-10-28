bdbb50360a96570d7b80865ab536964b22b6dab4
Satvik2Sharma <satviksharma1706@gmail.com>
2025-10-28 11:32:42 +0530

    Update replace space with underscore.py

--- replace space with underscore.py
+++ replace space with underscore.py
@@ -1,4 +1,4 @@
-# Using replace() method to replace space with underscore'_' in the given string.
+# sing replace() method to replace space with underscore'_' in the given string.
 
 s = input("Enter string: ")
 print(s.replace(" ", "_"))
