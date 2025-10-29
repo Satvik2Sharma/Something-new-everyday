79de74eda6195546d16f8480983e9f5bc91520a4
Satvik2Sharma <satviksharma1706@gmail.com>
2025-10-29 16:14:08 +0530

    Add function to remove duplicates from a list
    Implement a method to remove duplicates from a list.

---
+++ remove duplicates.py
@@ -0,0 +1,10 @@
+# Using for loop and append() method.
+# To remove duplicates from a list without using sets.
+
+
+alist = list(map(int, input("Enter list: ").split()))
+res = []
+for i in alist:
+    if i not in res:
+        res.append(i)
+print(res)
