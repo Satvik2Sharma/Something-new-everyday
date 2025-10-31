3e7a63da1c4cfaadfc82fc683f580d8b402511ac
Satvik2Sharma <satviksharma1706@gmail.com>
2025-11-01 01:10:52 +0530

    Create merge two lists.py

---
+++ merge two lists.py
@@ -0,0 +1,14 @@
+# Getting input strings of numbers separated by spaces for two lists and convert them to lists of integers.
+a = list(map(int, input("Enter list1: ").split()))
+b = list(map(int, input("Enter list2: ").split()))
+
+# Initialising an empty list to store the merged list without duplicates.
+c = []
+
+# Using for loop to Iterate through the elements of both lists.
+for i in a+b:
+# If the element is not already in the result list, using append() method.
+    if i not in c:
+        c.append(i)
+
+print(c)
