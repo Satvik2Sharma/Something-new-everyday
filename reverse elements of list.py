1dcfbde8f580729dd3c1fdca568bf5aee2993a3e
Satvik2Sharma <satviksharma1706@gmail.com>
2025-11-02 09:47:57 +0530

    Add script to reverse elements of a list

---
+++ reverse elements of list.py
@@ -0,0 +1,8 @@
+# Get input string from the user and split it into a list of strings
+a = input("Enter elements separated by spaces: ").split()
+
+# Reversing the list using slicing
+a_reversed = a[::-1]
+
+# Print the reversed list
+print(a_reversed)
