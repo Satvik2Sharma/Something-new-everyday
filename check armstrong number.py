f58b4aaf1a17948e0a49f71d6c109db28d58e993
Satvik2Sharma <satviksharma1706@gmail.com>
2025-11-05 21:01:02 +0530

    Remove print statements for Armstrong number check
    Removed the output statements for Armstrong number check.

--- check armstrong number.py
+++ check armstrong number.py
@@ -5,7 +5,3 @@ while temp > 0:
     digit = temp % 10         # get last digit
     sum += digit ** 3         # cube it and add to sum
     temp //= 10               # remove last digit
-if num == sum:
-    print(num, "is an Armstrong number 💪")
-else:
-    print(num, "is not an Armstrong number ❌")
