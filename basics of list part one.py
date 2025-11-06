58eba1ef16f9d2739bb889c218ca292f93ace1fa
Satvik2Sharma <satviksharma1706@gmail.com>
2025-11-06 23:30:02 +0530

    Add basic list operations in Python
    Demonstrate list operations including indexing, slicing, and appending.

---
+++ basics of list part one.py
@@ -0,0 +1,14 @@
+my_list = [10, 20, 30, 40, 50, 60]
+
+#PRINTING 3RD ITEM IN THE LIST
+print("Printing third element:", my_list[2])
+
+# Print the last two elements of my_list using slicing.
+print("Last two elements of the list my_list are:", my_list[4:6])
+
+# Using negative indexing to print the first and last elements of my_list.
+print("First element and last elements:", my_list[-6], my_list[-1])
+
+# Adding the element 25 to the end of the list using append(). Print the updated list.
+my_list.append(25)
+print("After adding element 25 to the end of the my_list:", my_list)
