85daf3ac233b29e147e518fa404a75d407f68a86
Satvik2Sharma <satviksharma1706@gmail.com>
2025-11-09 22:25:54 +0530

    Add input prompts for three numbers

--- find max min among 3 numbers.py
+++ find max min among 3 numbers.py
@@ -1,6 +1,6 @@
 # Write a program to find the maximum and minimum of three numbers using nested if-else.
 
-
+ 
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
