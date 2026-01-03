dad199cb6d270808efe7947b87f8b2052cf0f1b3
Satvik2Sharma <satviksharma1706@gmail.com>
2026-01-04 00:02:52 +0530

    Create fill-in-the-blanks story game

---
+++ story_fill-up.py
@@ -0,0 +1,13 @@
+print("Fill in the Blanks Story Game \n")
+
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb (ending with -ing): ")
+
+print("\n The Story:\n")
+print("One day, " + name + " was walking through " + place + ".")
+print("It was a very " + adjective + " day.")
+print("Suddenly, a " + noun + " appeared and started " + verb + ".")
+print(name + " couldn't believe it, and that moment changed everything!")
